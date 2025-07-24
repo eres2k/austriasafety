28c9803a70e29edd3c2aca144c557c3993a5c9ee
eres2k <eeesener@amazon.com>
2025-07-24 21:20:40 +0200

    Add files via upload

---
+++ netlify/functions/questionnaire-list.js
@@ -0,0 +1,22 @@
+import { HandlerContext } from '@netlify/functions';
+import { getStore } from '@netlify/blobs';
+
+export const handler = async (event: any, context: HandlerContext) => {
+  try {
+    const questionnaireStore = getStore('questionnaires', {
+      siteId: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_AUTH_TOKEN
+    });
+    const { blobs } = await questionnaireStore.list();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(blobs)
+    };
+  } catch (err) {
+    console.error(err.stack || err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message || err })
+    };
+  }
+};
