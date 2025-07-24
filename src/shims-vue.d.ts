e8a4fbce6945540d9d3999eb1cd2b552dd04b2a3
eres2k <eeesener@amazon.com>
2025-07-24 17:49:57 +0200

    Create shims-vue.d.ts

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '@heroicons/vue/24/solid'
+declare module '@heroicons/vue/24/outline'
+declare module '@heroicons/vue/20/solid'
